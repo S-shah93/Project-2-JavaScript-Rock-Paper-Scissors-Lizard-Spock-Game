6ebac879bf1b7d592cffb5c434bae7bc336a287d
S-shah93 <sumair.shah@hotmail.com>
2022-09-05 11:58:52 +0000

    Check if js is working, connected js link revealed bug with font awesome link, changed now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,1 @@
-const computerChoice = document.getElementById('computer-choice')
-const playerChoice = document.getElementById('player-choice')
-const resultDisplay = document.getElementById('result')
+console.log("hello")
