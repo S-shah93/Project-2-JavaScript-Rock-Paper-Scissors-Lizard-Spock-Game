42c06a8f6a77e8be819ab14acebbab8f19ab03ac
S-shah93 <sumair.shah@hotmail.com>
2022-09-08 15:21:24 +0000

    Add game and playgame function with query selector for buttons

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/* Entire Game logic inside this Function */
+const game = () => {
+    let playerScore = 0;
+  
+    let computerScore = 0;
+  
+    let moves = 0;
+/* Function to start playing game   */
+
+    const playGame = () => {
+        const rockBtn = document.querySelector(".rock");
+        const paperBtn = document.querySelector(".paper");
+        const scissorBtn = document.querySelector(".scissor");
+        const lizardBtn = document.querySelector(".lizard");
+        const spockBtn = document.querySelector(".spock");
+
+        const playerOptions = [rockBtn, paperBtn, scissorBtn, lizardBtn, spockBtn];
+        const computerOptions = ["rock", "paper", "scissor", "lizard", "spock"];
+    }
+}
